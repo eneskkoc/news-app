9109f2405051385b41d318a74a7a520407d46178
eneskkoc <eneskkoc@hotmail.com>
2021-11-03 01:10:26 +0300

    app commit

--- app/src/main/java/com/example/navigation/util/Constants.kt
+++ app/src/main/java/com/example/navigation/util/Constants.kt
@@ -2,7 +2,7 @@ package com.example.navigation.util
 
 object Constants {
     const val API_BASE_PATH = "https://newsapi.org/v2/"
-    const val API_KEY = "aba85326a278492f8ca2a2f170f48aae"
+    const val API_KEY = "get api key --> https://currentsapi.services/en"
     const val Country = "tr"
 
 }
